a7ac4481d3e5858e5c75bc3e0e5c9df94d2389e6
Stuckdownawell <stuckdownawell@gmail.com>
2015-01-22 10:49:18 +0000

    Remove " from escaping code

--- index.js
+++ index.js
@@ -4,8 +4,30 @@ var gutil = require('gulp-util');
 var concat = require('gulp-concat');
 var header = require('gulp-header');
 var footer = require('gulp-footer');
-var htmlJsStr = require('js-string-escape');
+//var htmlJsStr = require('js-string-escape');
 
+//TODO make a configuration escaper but for now just remove "
+var htmlJsStr = function (string) {
+  return ('' + string).replace(/['\\\n\r\u2028\u2029]/g, function (character) {
+    // Escape all characters not included in SingleStringCharacters and
+    // DoubleStringCharacters on
+    // http://www.ecma-international.org/ecma-262/5.1/#sec-7.8.4
+    switch (character) {    
+      case "'":
+      case '\\':
+        return '\\' + character
+      // Four possible LineTerminator characters need to be escaped:
+      case '\n':
+        return '\\n'
+      case '\r':
+        return '\\r'
+      case '\u2028':
+        return '\\u2028'
+      case '\u2029':
+        return '\\u2029'
+    }
+  })
+}
 /**
  * "constants"
  */
